5f63ec9f06cd2f8d1a4bd73d67df75955fe04ec6
Nathan Kerr <nathan@nathankerr.com>
2012-10-30 19:15:34 +0100

    prototype: generate_server tranformation now more general

--- generate_server.go
+++ generate_server.go
@@ -4,12 +4,35 @@ import (
 	"strings"
 )
 
+
+// not in seed: 000, 010, 0n0, 100, n00
+// unknowable send_to_addr: 011, 01n, 0n1, 0nn
 var generate_server = seedToBudTransformations{
-	"101": generate_server_101,
-	"111": generate_server_111,
+	"001": generate_server_add_clients,
+	"00n": generate_server_add_clients,
+	// "011": no_s2b,
+	// "01n": no_s2b,
+	// "0n1": no_s2b,
+	// "0nn": no_s2b,
+	"101": generate_server_add_clients,
+	"10n": generate_server_add_clients,
+	"110": generate_server_add_clients,
+	"111": generate_server_add_clients,
+	"11n": generate_server_add_clients,
+	"1n0": generate_server_add_clients,
+	"1n1": generate_server_add_clients,
+	"1nn": generate_server_add_clients,
+	"n01": generate_server_add_clients,
+	"n0n": generate_server_add_clients,
+	"n10": generate_server_add_clients,
+	"n11": generate_server_add_clients,
+	"n1n": generate_server_add_clients,
+	"nn0": generate_server_add_clients,
+	"nn1": generate_server_add_clients,
+	"nnn": generate_server_add_clients,
 }
 
-func generate_server_101(buds budCollection, cluster *cluster, seed *seed, sname string) budCollection {
+func generate_server_add_clients(buds budCollection, cluster *cluster, seed *seed, sname string) budCollection {
 	transformationinfo()
 
 	sname = strings.Title(sname) + "Server"
@@ -19,49 +42,25 @@ func generate_server_101(buds budCollection, cluster *cluster, seed *seed, sname
 		bud = newBud()
 	}
 
-	// as the inputs and outputs are already
-	// projected, there is no need to remove the
-	// address columns added to the interfaces
-	// when converting them to channels
+	// create a list of output_addrs
+	output_addrs := []string{}
 	for name, _ := range cluster.collections {
 		collection := seed.collections[name]
-		switch collection.typ {
-		case seedInput:
-			channel := seedTableToBudTable(name, budChannel, collection)
-			bud.collections[name] = channel
-		case seedOutput:
-			output := seedTableToBudTable(name, budScratch, collection)
-			bud.collections[name] = output
-		case seedTable:
-			table := seedTableToBudTable(name, budPersistant, collection)
-			bud.collections[name] = table
+		if collection.typ == seedOutput {
+			output_addrs = append(output_addrs, name + "_addr")
 		}
-
-		buds[sname] = bud
 	}
 
-	for _, rule := range cluster.rules {
-		bud.rules = append(bud.rules, seed.rules[rule])
-	}
-
-	return buds
-}
-
-func generate_server_111(buds budCollection, cluster *cluster, seed *seed, sname string) budCollection {
-	transformationinfo()
-
-	sname = strings.Title(sname) + "Server"
-
-	bud, ok := buds[sname]
-	if !ok {
-		bud = newBud()
-	}
-
+	// process the collections
 	for name, _ := range cluster.collections {
 		collection := seed.collections[name]
 		switch collection.typ {
 		case seedInput:
-			key := []string{"client"}
+			// add output_addrs to the beginning of key
+			key := []string{}
+			for _, output_addr := range output_addrs {
+				key = append(key, output_addr)
+			}
 			for _, ckey := range collection.key {
 				key = append(key, ckey)
 			}
@@ -71,7 +70,7 @@ func generate_server_111(buds budCollection, cluster *cluster, seed *seed, sname
 			bud.collections[name] = input
 		case seedOutput:
 			output := seedTableToBudTable(name, budChannel, collection)
-			output.key[0] = "@client"
+			output.key[0] = "@" + name + "_addr"
 			bud.collections[name] = output
 		case seedTable:
 			table := seedTableToBudTable(name, budPersistant, collection)
@@ -83,22 +82,42 @@ func generate_server_111(buds budCollection, cluster *cluster, seed *seed, sname
 		buds[sname] = bud
 	}
 
+	// process the rules
 	for _, rulenum := range cluster.rules {
 		rule := seed.rules[rulenum]
 
-		// find the input channel and add its client to the beginning of output
-		if seed.collections[rule.supplies].typ == seedOutput {
-			rule.typ = ruleAsyncInsert
-			for collection, _ := range rule.requires {
-				if seed.collections[collection].typ == seedInput {
-					output := []qualifiedColumn{qualifiedColumn{collection: collection, column: "client"}}
-					for _, o := range rule.output {
-						output = append(output, o)
-					}
-					rule.output = output
-					break
-				}
+		inputs := []string{}
+		for name, _ := range rule.requires {
+			if seed.collections[name].typ == seedInput {
+				inputs = append(inputs, name)
 			}
+		}
+
+		// add predicates when needed
+		for i := 1; i < len(inputs); i++ {
+			for _, output_addr := range output_addrs {
+				rule.predicates = append(rule.predicates, predicate{
+					left: qualifiedColumn{collection: inputs[0], column: output_addr},
+					right: qualifiedColumn{collection: inputs[i], column: output_addr},
+					})
+			}
+		}
+
+		switch seed.collections[rule.supplies].typ {
+		case seedOutput:
+			rule.typ = ruleAsyncInsert
+			output := []qualifiedColumn{}
+			if len(inputs) > 0 {
+				output = append(output, qualifiedColumn{collection: inputs[0], column: rule.supplies + "_addr"})
+			}
+			for _, o := range rule.output {
+				output = append(output, o)
+			}
+			rule.output = output
+		case seedTable:
+			// no-op
+		default:
+			panic("shouldn't get here")
 		}
 
 		bud.rules = append(bud.rules, rule)
