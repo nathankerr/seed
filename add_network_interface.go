fc79eeb654b9266c10db59a14c60aaa7b01c99da
Nathan Kerr <nathan@nathankerr.com>
2012-12-03 22:51:16 +0100

    prototype: readded groups, also confined to the transformation file

--- add_network_interface.go
+++ add_network_interface.go
@@ -1,30 +1,140 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 )
 
+func add_network_interface(sname string, seed *service, services map[string]*service) map[string]*service {
+	groups := getGroups(sname, seed)
+
+	for _, group := range groups {
+		switch group.typ() {
+		case "000", "010", "0n0", "100", "n00": // not possible
+			panic(group.typ())
+		case "011", "01n", "0n1", "0nn": // not input driven (not handled)
+			panic(group.typ())
+		case "001", "00n", "101", "10n", "n01", "n0n", // passthrough
+			"110", "111", "11n", // single output
+			"1n0", "1n1", "1nn", // multiple output
+			"n10", "n11", "n1n", "nn0", "nn1", "nnn": // multiple input
+			services = add_network_interface_helper(services, group, seed, sname)
+		default:
+			// shouldn't get here
+			panic(group.typ())
+		}
+	}
+
+	return services
+}
+
+type group struct {
+	rules       []int
+	collections map[string]collectionType
+}
+
+func getGroups(sname string, seed *service) map[string]*group {
+	groups := make(map[string]*group)
+	collectionToGroupMap := make(map[string]string)
+
+	for num, rule := range seed.Rules {
+		// find or create the group, ref with groupName
+		groupName := ""
+		for _, collection := range rule.collections() {
+			// tables are not included in the group
+			if seed.Collections[collection].Type == collectionTable {
+				continue
+			}
+
+			name, ok := collectionToGroupMap[collection]
+			if ok {
+				groupName = name
+				break
+			}
+		}
+		if groupName == "" {
+			groupName = fmt.Sprintf("%s%d", sname, rule.Source.Line)
+			collections := make(map[string]collectionType)
+			groups[groupName] = &group{collections: collections}
+		}
+
+		// add the rule
+		groups[groupName].rules = append(groups[groupName].rules, num)
+
+		// add the relevant collections
+		collectionToGroupMap[rule.Supplies] = groupName
+		groups[groupName].collections[rule.Supplies] =
+			seed.Collections[rule.Supplies].Type
+
+		for _, cname := range rule.collections() {
+			collectionToGroupMap[cname] = groupName
+			groups[groupName].collections[cname] = seed.Collections[cname].Type
+		}
+	}
+
+	return groups
+}
+
+func (g *group) typ() string {
+	var inputs, outputs, tables int
+
+	for _, ctyp := range g.collections {
+		switch ctyp {
+		case collectionInput:
+			inputs++
+		case collectionOutput:
+			outputs++
+		case collectionTable:
+			tables++
+		default:
+			// shouldn't get here
+			panic(ctyp)
+		}
+	}
+
+	return fmt.Sprint(count(inputs), count(outputs), count(tables))
+}
+
+func count(i int) string {
+	var str string
+	switch {
+	case i == 0:
+		str = "0"
+	case i == 1:
+		str = "1"
+	case i > 1:
+		str = "n"
+	default:
+		// shouldn't get here
+		panic(i)
+	}
+	return str
+}
+
 // adds a network interface by adding and handling explicit correlation data
-func add_network_interface(sname string, seed *service, transformed map[string]*service) map[string]*service {
+func add_network_interface_helper(buds map[string]*service, group *group,
+	seed *service, sname string) map[string]*service {
 	info()
 
 	sname = strings.Title(sname) + "Server"
 
-	new_seed, ok := transformed[sname]
+	bud, ok := buds[sname]
 	if !ok {
-		new_seed = &service{Collections: make(map[string]*collection), Source: seed.Source}
+		bud = &service{Collections: make(map[string]*collection), Source: seed.Source}
 	}
 
 	// name the output address columns
 	output_addrs := []string{}
-	for name, collection := range seed.Collections {
+	for name, _ := range group.collections {
+		collection := seed.Collections[name]
 		if collection.Type == collectionOutput {
 			output_addrs = append(output_addrs, name+"_addr")
 		}
 	}
 
 	// Add correlation information to the collections
-	for name, collection := range seed.Collections {
+	for name, _ := range group.collections {
+		collection := seed.Collections[name]
 		switch collection.Type {
 		case collectionInput:
 			// add output_addrs to the beginning of key
@@ -48,13 +158,15 @@ func add_network_interface(sname string, seed *service, transformed map[string]*
 			// should not get here
 			panic(collection.Type)
 		}
-		new_seed.Collections[name] = collection
+		bud.Collections[name] = collection
 
-		transformed[sname] = new_seed
+		buds[sname] = bud
 	}
 
 	// rewrite the rules to take the correlation data into account
-	for _, rule := range seed.Rules {
+	for _, rulenum := range group.rules {
+		rule := seed.Rules[rulenum]
+
 		inputs := []string{}
 		for _, name := range rule.collections() {
 			if seed.Collections[name].Type == collectionInput {
@@ -85,8 +197,7 @@ func add_network_interface(sname string, seed *service, transformed map[string]*
 			if len(inputs) > 0 {
 				projection = append(projection, qualifiedColumn{
 					Collection: inputs[0],
-					Column:     rule.Supplies + "_addr",
-				})
+					Column:     rule.Supplies + "_addr"})
 			}
 			for _, o := range rule.Projection {
 				projection = append(projection, o)
@@ -99,15 +210,15 @@ func add_network_interface(sname string, seed *service, transformed map[string]*
 			panic(seed.Collections[rule.Supplies].Type)
 		}
 
-		new_seed.Rules = append(new_seed.Rules, rule)
+		bud.Rules = append(bud.Rules, rule)
 	}
 
-	// convert the inputs and outputs into channels
-	for _, collection := range new_seed.Collections {
+	for cname, _ := range group.collections {
+		collection := seed.Collections[cname]
 		switch collection.Type {
 		case collectionInput, collectionOutput:
 			collection.Type = collectionChannel
-		case collectionChannel, collectionTable:
+		case collectionTable:
 			// no-op
 		default:
 			// shouldn't get here
@@ -115,5 +226,5 @@ func add_network_interface(sname string, seed *service, transformed map[string]*
 		}
 	}
 
-	return transformed
+	return buds
 }
