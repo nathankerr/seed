fcf1645f463ff6f8ab6748351d493d0bed42158f
Nathan Kerr <nathan@nathankerr.com>
2012-12-20 22:45:58 +0100

    various changes dealing with blocks

--- src/service/ruby.go
+++ src/service/ruby.go
@@ -15,28 +15,36 @@ func (s *Service) toRuby(name string) string {
 
 	name = strings.Title(name)
 	str := fmt.Sprintf("module %s\n", name)
+	collections := s.Collections
+
+	rules := fmt.Sprintf("\n  bloom do\n")
+	for rule_num, rule := range s.Rules {
+		rule_str, additional_collections := rule.Ruby(s)
+		rules = fmt.Sprintf("%s    %s #%s rule %d\n", rules, rule_str, rule.Source, rule_num)
+		for cname, collection := range additional_collections {
+			collections[cname] = collection
+		}
+	}
+	rules = fmt.Sprintf("%s  end\n", rules)
 
 	str = fmt.Sprintf("%s  state do\n", str)
-	for cname, collection := range s.Collections {
+	for cname, collection := range collections {
 		str = fmt.Sprintf("%s    %s #%s\n", str, collection.Ruby(cname),
 			collection.Source)
 	}
 	str = fmt.Sprintf("%s  end\n", str)
 
-	str = fmt.Sprintf("%s\n  bloom do\n", str)
-	for rule_num, rule := range s.Rules {
-		str = fmt.Sprintf("%s    %s #%s rule %d\n", str, rule.Ruby(), rule.Source, rule_num)
-	}
-	str = fmt.Sprintf("%s  end\n", str)
+	str += rules
 
 	str = fmt.Sprintf("%send\n", str)
 
 	return str
 }
 
-func (r *Rule) Ruby() string {
+func (r *Rule) Ruby(service *Service) (string, map[string]*Collection) {
 	var selecter string
 
+	additional_collections := make(map[string]*Collection)
 	collections := r.Requires()
 
 	index := make(map[string]string)
@@ -54,7 +62,7 @@ func (r *Rule) Ruby() string {
 	}
 
 	if len(collections) == 1 {
-		selecter = fmt.Sprintf("%s do |%s| [%s] end",
+		selecter = fmt.Sprintf("%s do |%s|\n      [%s]\n    end",
 			r.Projection[0].Collection,
 			strings.Join(names, ", "),
 			strings.Join(output, ", "))
@@ -64,7 +72,7 @@ func (r *Rule) Ruby() string {
 			predicates = append(predicates, p.String())
 		}
 
-		selecter = fmt.Sprintf("(%s).combos(%s) do |%s| [%s] end",
+		selecter = fmt.Sprintf("(%s).combos(%s) do |%s|\n      [%s]\n    end",
 			strings.Join(collections, " * "),
 			strings.Join(predicates, ", "),
 			strings.Join(names, ", "),
@@ -79,53 +87,75 @@ func (r *Rule) Ruby() string {
 			len(filepath.Ext(r.Source.Name))]
 		scratch_name = fmt.Sprintf("%s%d_scratch",
 			scratch_name, r.Source.Line)
-		scratch := fmt.Sprintf("temp :%s <= %s #%s",
+		scratch_collection := &Collection{
+			Type: CollectionScratch,
+			// Key: service.Collections[r.Supplies].Key,
+			// Data: service.Collections[r.Supplies].Data,
+			Source: r.Source,
+		}
+		// for _, column := range service.Collections[r.Supplies].Key {
+		// 	scratch_collection.Key = append(scratch_collection.Key,
+		// 		strings.Replace(column, "@", "", -1))
+		// }
+		for _, column := range r.Projection {
+			scratch_collection.Key = append(scratch_collection.Key,
+				column.Column)
+		}
+		additional_collections[scratch_name] = scratch_collection
+		scratch := fmt.Sprintf("%s <= %s #%s",
 			scratch_name, selecter, r.Source)
-		indented_block := strings.Replace(r.Block, "\n", "\n\t\t", -1)
+		indented_block := strings.Replace(r.Block, "\n", "\n    ", -1)
+		indented_block = strings.Replace(indented_block, "\t", "  ", -1)
 		if r.Block[0] == 'm' {
 			// map block
-			return fmt.Sprintf("%s\n\t\t%s %s %s %s",
-				scratch, r.Supplies, r.Operation, scratch_name, indented_block)
+			return fmt.Sprintf("%s\n    %s %s %s do %s",
+					scratch, r.Supplies, r.Operation, scratch_name, indented_block[4:]),
+				additional_collections
 		} else {
 			// reduce block
-			return fmt.Sprintf("%s\n\t\t%s %s %s.reduce({}) do %s", scratch,
-				r.Supplies, r.Operation, scratch_name, indented_block[7:])
+			return fmt.Sprintf("%s\n    %s %s %s.reduce({}) do %s", scratch,
+					r.Supplies, r.Operation, scratch_name, indented_block[7:]),
+				additional_collections
 		}
 	}
 
 	return fmt.Sprintf("%s %s %s",
-		r.Supplies,
-		r.Operation,
-		selecter)
+			r.Supplies,
+			r.Operation,
+			selecter),
+		additional_collections
 }
 
 func (c *Collection) Ruby(name string) string {
-	declaration := ""
+	var declaration string
 
 	switch c.Type {
 	case CollectionInput:
-		declaration += "interface input,"
+		declaration = "interface input,"
 	case CollectionOutput:
-		declaration += "interface output,"
+		declaration = "interface output,"
 	case CollectionChannel:
-		declaration += "channel"
+		declaration = "channel"
 	case CollectionTable:
-		declaration += "table"
+		declaration = "table"
 	case CollectionScratch:
-		declaration += "scratch"
+		declaration = "scratch"
 	default:
 		// shouldn't get here
 		panic(c.Type)
 	}
 
-	declaration += fmt.Sprintf(" :%s, [", name)
+	declaration = fmt.Sprintf("%s :%s, [", declaration, name)
 
 	for _, v := range c.Key {
 		declaration += fmt.Sprintf(":%s, ", v)
 	}
+	if len(c.Key) != 0 {
+		declaration = declaration[:len(declaration)-2]
+	}
 
 	if len(c.Data) > 0 {
-		declaration = declaration[:len(declaration)-2] + "] => ["
+		declaration += "] => ["
 
 		for _, v := range c.Data {
 			declaration += fmt.Sprintf(":%s, ", v)
@@ -133,7 +163,7 @@ func (c *Collection) Ruby(name string) string {
 
 		declaration = declaration[:len(declaration)-2] + "]"
 	} else {
-		declaration = declaration[:len(declaration)-2] + "]"
+		declaration += "]"
 	}
 
 	return declaration
