cc6efc27b4ce546fe719bf310c333fddf695bffc
Nathan Kerr <nathan@nathankerr.com>
2013-01-04 00:44:57 +0100

    indexesFor seems to work

--- src/executor/rules.go
+++ src/executor/rules.go
@@ -4,8 +4,92 @@ import (
 	"errors"
 )
 
-func productNumberToIndexes(productNumber int, lengths []int) []int {
-	return nil
+func indexesFor(productNumber int, lengths []int) ([]int, error) {
+	// productNumber must be non-negative
+	if productNumber < 0 {
+		return nil, errors.New("productNumber must be non-negative")
+	}
+
+	// lengths must be positive
+	for _, length := range lengths {
+		if length <= 0 {
+			return nil, errors.New("lengths must be positive")
+		}
+	}
+	
+	// productNumber cannot be greater than the number of possible products
+	numberOfProducts1, err := numberOfProducts(lengths)
+	if err != nil {
+		return nil, err
+	}
+	if productNumber > numberOfProducts1 {
+		return nil, errors.New("productNumber is greater than the number of possible products")
+	}
+
+	// this is sort of a reverse base coversion which starts with
+	// the least significant digit
+	// indexes holds the digits, indexes[0] is least significant
+	indexes := make([]int, 0)
+	for i, length := range lengths {
+		index := productNumber
+
+		// find out how many products have already been
+		// represented and remove them from consideration by
+		// this index
+		productsBeforeThis, err := productNumberFor(indexes, lengths[:i])
+		if err != nil {
+			panic(err)
+		}
+		index -= productsBeforeThis
+
+		// now the index is just a factor off
+		// find the factor and remove it
+		factor, err := numberOfProducts(lengths[:i])
+		if err != nil {
+			panic(err)
+		}
+		index /= factor
+
+		// now limit the index to what it can hold (length)
+		// any remaining part of productNumber will be
+		// handled by the next index
+		if index >= length {
+			index %= length
+		}
+
+		indexes = append(indexes, index)
+	}
+
+	return indexes, nil
+}
+
+func productNumberFor(indexes []int, lengths []int) (int, error) {
+	// indexes and arrays must be the same length
+	if len(indexes) != len(lengths) {
+		return -1, errors.New("indexes and lengths must have the same lengths")
+	}
+
+	// check ranges for indexes and lengths
+	for i, _ := range indexes {
+		if indexes[i] < 0 {
+			return -1, errors.New("indexes must be non-negative")
+		}
+		if lengths[i] <= 0 {
+			return -1, errors.New("lengths must be positive")
+		}
+	}
+
+	productNumber := 0
+	for i, _ := range indexes {
+		factor, err := numberOfProducts(lengths[:i])
+		if err != nil {
+			panic(err)
+		}
+
+		productNumber += factor * indexes[i]
+	}
+
+	return productNumber, nil
 }
 
 
--- src/executor/rules_test.go
+++ src/executor/rules_test.go
@@ -41,23 +41,141 @@ func TestNumberOfProductsQuick (t *testing.T) {
 		products, err := numberOfProducts(lengths)
 
 		if containsNonPositive {
-			return err != nil //&& err.Error() == "lengths must be positive"
-		} else {
-			for _, length := range lengths {
-				products /= length
-			}
-			return products == 1
+			return err != nil && err.Error() == "lengths must be positive"
 		}
-		return false
+
+		for _, length := range lengths {
+			products /= length
+		}
+		return products == 1
 	}
 	if err := quick.Check(f, nil); err != nil {
 		t.Error(err)
 	}
 }
 
-func TestProductNumberToIndexes (t *testing.T) {
-	numberOfProducts := 0
+func TestIndexesFor (t *testing.T) {
+	checkExpectedProductNumberIndexes(
+		map[int][]int{
+			0: []int{0},
+			1: []int{1},
+			2: []int{2},
+		},
+		[]int{3},
+		t,
+	)
+
+	checkExpectedProductNumberIndexes(
+		map[int][]int{
+			0: []int{0, 0},
+			1: []int{1, 0},
+			2: []int{2, 0},
+			3: []int{0, 1},
+			4: []int{1, 1},
+			5: []int{2, 1},
+			6: []int{0, 2},
+			7: []int{1, 2},
+			8: []int{2, 2},
+		},
+		[]int{3, 3},
+		t,
+	)
+}
+
+func checkExpectedProductNumberIndexes(expected map[int][]int, lengths []int, t *testing.T) {
+	numberOfProducts, err := numberOfProducts(lengths)
+	if err != nil {
+		t.Error(err)
+	}
 	for productNumber := 0; productNumber < numberOfProducts; productNumber++ {
-		// productNumberToIndexes(productNumber int, lengths []int)
+		indexes, err := indexesFor(productNumber, lengths)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(indexes) != len(lengths) {
+			t.Errorf("%v != %v", len(indexes), len(lengths))
+		}
+		for i, index := range indexes {
+			if index != expected[productNumber][i] {
+				t.Errorf("%v != %v for productNumber %v", indexes, expected[productNumber], productNumber)
+				break
+			}
+		}
+	}
+}
+
+func TestIndexesForQuick (t *testing.T) {
+	f := func(productNumber int, lengths []int) bool {
+		indexes, err := indexesFor(productNumber, lengths)
+
+		if err != nil {
+			switch err.Error() {
+			case "productNumber must be non-negative":
+				if productNumber < 0 {
+					return true
+				}
+				return false
+			case "lengths must be positive":
+				containsNonPositive := false
+				for _, length := range lengths {
+					if length <= 0 {
+						containsNonPositive = true
+						break
+					}
+				}
+
+				if containsNonPositive {
+					return true
+				}
+				return false
+			case "productNumber is greater than the number of possible products":
+				return true
+			default:
+				return false
+			}
+			return false
+		}
+
+		// there should be the same number of indexes as lengths
+		if len(indexes) != len(lengths) {
+			return false
+		}
+
+		// reconstruct the product number from the indexes
+		expectedProductNumber := 0
+		for i, index := range indexes {
+			expectedProductNumber += lengths[i] * index
+		}
+
+		return expectedProductNumber == productNumber
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestProductNumberFor (t *testing.T) {
+	lengths := []int{3,3}
+	expected := map[int][]int{
+		0: []int{0, 0},
+		1: []int{1, 0},
+		2: []int{2, 0},
+		3: []int{0, 1},
+		4: []int{1, 1},
+		5: []int{2, 1},
+		6: []int{0, 2},
+		7: []int{1, 2},
+		8: []int{2, 2},
+	}
+
+	for expectedProductNumber, indexes := range expected {
+		productNumber, err := productNumberFor(indexes, lengths)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if productNumber != expectedProductNumber {
+			t.Errorf("%v != %v for %v", productNumber, expectedProductNumber, indexes)
+		}
 	}
 }
