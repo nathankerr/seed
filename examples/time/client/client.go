5485a1242d747887f08af7fc96d8debf6e4047e9
Nathan Kerr <nathan@nathankerr.com>
2013-02-25 11:56:03 +0100

    clean up

--- examples/time/client/client.go
+++ examples/time/client/client.go
@@ -1,13 +1,14 @@
 package main
 
-import(
+import (
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 
-	err := http.ListenAndServe(":4000", nil)
+	println("Starting time client on http://127.0.0.1:4000")
+	err := http.ListenAndServe("127.0.0.1:4000", nil)
 	if err != nil {
 		println(err.Error())
 	}
