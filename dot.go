b83203bcbc84cb8d545a74e885fd314456307587
Nathan Kerr <nathan@nathankerr.com>
2013-04-08 19:21:18 +0200

    removed page setting for graphviz output as the behavior affected by the parameter changed

--- dot.go
+++ dot.go
@@ -11,7 +11,6 @@ func ToDot(seed *Seed, name string) ([]byte, error) {
 	dot := fmt.Sprintf("digraph %s {", name)
 	dot = fmt.Sprintf("%s\n\tmargin=\"0\"", dot)
 	dot = fmt.Sprintf("%s\n\tsize=\"4.5,7.1\"", dot)
-	dot = fmt.Sprintf("%s\n\tpage=\"324,12\"", dot)
 	dot = fmt.Sprintf("%s\n\tnode [fontname=\"Alegreya\" fontsize=\"9\"]", dot)
 	dot = fmt.Sprintf("%s\n", dot)
 
